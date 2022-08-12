45dd5d2964057761c744d94bfd27132c67a4c350
St. Elmo Wilken <stelmozors@gmail.com>
2022-08-12 21:23:45 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/BiGGReactions.jl
@@ -0,0 +1,5 @@
+module BiGGReactions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BiGGReactions
+using Test
+
+@testset "BiGGReactions.jl" begin
+    # Write your tests here.
+end
