4bf88b4ce31cf7ea3f6e6c71af663576f553cb0f
St. Elmo Wilken <stelmozors@gmail.com>
2022-08-27 21:07:00 +0200

    clear cache for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using BiGGReactions
 using Test
 
 @testset "BiGGReactions.jl" begin
-    BiGGReactions.clear_cache!()
+    clear_cache!()
     include("reactions.jl")
     include("metabolites.jl")
 end
